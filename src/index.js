ff1d2fd27a1b641598f34dfd07cdc84943b55c30
Bryan Clover <bclover09@gmail.com>
2018-09-22 06:22:10 -0400

    test react-router setup

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import App from './components/app';
 import reducers from './reducers';
@@ -9,15 +10,24 @@ import reducers from './reducers';
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 class Hello extends React.Component {
-  render() { return <div>Hello!</div> }
+  render() {
+    return <div>Hello!</div>
+  }
 }
 
 class Goodbye extends React.Component {
-  render() { return <div>Goodbye!</div> }
+  render() {
+    return <div>Goodbye!</div>
+  }
 }
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <div>
+        <Route path="/hello" component={Hello}/>
+        <Route path="/goodbye" component={Goodbye}/>
+      </div>
+    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
