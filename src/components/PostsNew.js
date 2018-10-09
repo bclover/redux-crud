61164bb62f0200146b408de4149d7bd9c65ff00f
Bryan Clover <bclover09@gmail.com>
2018-10-09 10:10:19 -0400

    add eslint config and wire up onSubmit

--- src/components/PostsNew.js
+++ src/components/PostsNew.js
@@ -12,14 +12,22 @@ class PostsNew extends Component {
           {...field.input}
           type="text"
         />
+        <div className="text-danger">{field.meta.error}</div>
       </div>
     );
   }
 
+  onSubmit(values){
+    console.log('values:', values);
+  }
+
   render () {
+
+    const { handleSubmit } = this.props;
+
     return (
       <div>
-        <form>
+        <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
           <Field
             label="Title"
             name="title"
@@ -35,10 +43,34 @@ class PostsNew extends Component {
             name="content"
             component={this.renderField}
           />
+          <button type="submit" className="btn btn-primary">Submit</button>
         </form>
       </div>
     );
   }
 }
 
-export default reduxForm({form: 'PostsNewForm'})(PostsNew);
+function validate (values) {
+  const errors = {};
+
+  if (!values.title) {
+    errors.title = 'Please enter a title.';
+  }
+
+  if (!values.categories) {
+    errors.categories = 'Please enter some categories.';
+  }
+
+  if (!values.content) {
+    errors.content = 'Please enter some content.';
+  }
+
+  // If errors is an empty object, the form is fine to submit
+  // If errors has any properties in it, redux-form assumes the form is invalid
+  return errors;
+}
+
+export default reduxForm({
+  validate,
+  form: 'PostsNewForm',
+})(PostsNew);
